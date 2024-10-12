57f58953e2a8300a87fcbc1f59718354099fb53f
rouxianmantou <rouxianmantou@gmail.com>
2024-10-12 14:13:07 +0800

    [feat] add first node CheckValueTypeNode.

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from nodes.node_check_value_type import CheckValueTypeNode
+
+NODE_CLASS_MAPPINGS = {
+    "CheckValueTypeNode": CheckValueTypeNode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "CheckValueTypeNode": "🏸 Check Value Type"
+}
---
+++ nodes/node_check_value_type.py
@@ -0,0 +1,54 @@
+# Example Custom Node for ComfyUI
+# You would typically put this code in a Python file within the custom nodes directory of ComfyUI
+
+class CheckValueTypeNode:
+    """
+    A custom node for ComfyUI to check the type of the input value.
+
+    Class methods
+    -------------
+    INPUT_TYPES (dict):
+        Defines input parameters of the node.
+    
+    Attributes
+    ----------
+    RETURN_TYPES (`tuple`):
+        The type of each element in the output tuple.
+    FUNCTION (`str`):
+        The name of the entry-point method.
+    CATEGORY (`str`):
+        The category the node should appear in the UI.
+    execute(s) -> tuple || None:
+        The entry point method. The name of this method must be the same as the value of property `FUNCTION`.
+    """
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "input_data": ("*",),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "execute"
+    CATEGORY = "Utility"
+
+    def execute(self, input_data):
+        """
+        Checks the type of the input data and returns it as a string.
+        
+        Parameters:
+            input_data: The input data whose type needs to be checked.
+        
+        Returns:
+            output_type (str): A string representing the type of the input data.
+        """
+        output_type = str(type(input_data).__name__)
+        return (output_type,)
+
+
+
